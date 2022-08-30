8d812217752a833b7ba3cc3393cf82069cd2c5b0
chisa-f <kiki4537fer2cat@gmail.com>
2022-08-30 16:52:34 -0400

    Add Assignment 2 files

---
+++ Assignment2/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int nth = 0;
+        int num = 10;
+        nth = fibonacci(num);
+        System.out.println("The " + num + "th term of the Fibonacci sequence is " + nth);
+    }//end main method
+
+    public static int fibonacci(int n) {
+        if (n < 0) {
+            return -1;
+        } //when n is negative value
+        else if (n <= 1) {
+            return n;
+        }//when n is 0 or 1
+
+        //recursion
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }//end fibonacci method
+}//end Main Class
